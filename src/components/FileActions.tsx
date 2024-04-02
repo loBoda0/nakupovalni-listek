f72b9ebd46be626abba66ccd01a8ed54c1c04355
Martin <martin.loboda02@gmail.com>
2024-04-02 14:23:15 +0200

    export to json

--- src/components/FileActions.tsx
+++ src/components/FileActions.tsx
@@ -1,10 +1,27 @@
+'use client'
+
+import { getItems } from '@/actions/items';
 import React from 'react'
 import { MdFileUpload, MdFileDownload } from "react-icons/md";
 
 const FileActions = () => {
+  const exportData = async () => {
+    console.log('object')
+    const data = await getItems()
+    console.log(data)
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(data)
+    )}`;
+
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    link.click();
+  }
   return (
     <div className='flex h-48'>
-      <div className='w-1/2 flex items-center justify-center border-dashed border-2 m-1'>
+      <div className='w-1/2 flex items-center justify-center border-dashed border-2 m-1' onClick={exportData}>
         <MdFileDownload size={48} />
       </div>
       <div className='w-1/2 flex items-center justify-center border-dashed border-2 m-1'>
