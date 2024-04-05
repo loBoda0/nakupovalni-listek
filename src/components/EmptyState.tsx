63af2326c85148ce902062b80830f7d4b0ff42e4
Martin <martin.loboda02@gmail.com>
2024-04-05 10:38:56 +0200

    Empty state added

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmptyState = () => {
+  return (
+    <div className='flex items-center justify-center h-full'>
+      Add your first item to list
+    </div>
+  )
+}
+
+export default EmptyState
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import ListItem from './ListItem'
 import AddItem from './AddItem'
 import FileActions from './FileActions'
+import EmptyState from './EmptyState'
 
 interface ItemListProps {
   items: ShopItem[]
@@ -12,11 +13,19 @@ const ItemList: React.FC<ItemListProps> = ({ items }) => {
   return (
     <div className='bg-white rounded-md p-4 w-[412px] m-3'>
       <AddItem />
+      <div className="h-96 overflow-scroll">
       {
-        items.map(item => (
-          <ListItem key={item.id} item={item} />
-        ))
-      }
+        items.length > 0 ? 
+          <>
+            {
+              items.map(item => (
+                <ListItem key={item.id} item={item} />
+              ))
+            }
+          </>
+         : <EmptyState />
+        }
+      </div>
       <FileActions />
     </div>
   )
